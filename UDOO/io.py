e22327a3d46e074e69ab6d6085073263707ab79e
Marcos Lopez <feirlane@gmail.com>
2016-02-28 12:35:35 +0100

    Serialize sensor reads

--- UDOO/io.py
+++ UDOO/io.py
@@ -27,23 +27,30 @@ class SRF02(Sensor):
         except Exception as e:
             print(e)
 
-        thr = threading.Thread(target=self.readLoop, args=(), kwargs={})
+    def measure(self):
+        self.bus.write_byte_data(self.address, 0, 0x51)
+        time.sleep(0.2)
+        self.lastValue = self.bus.read_word_data(self.address, 0x02)
 
-        thr.start()
+        print self.lastValue,
+
+        if self.lastValue > 15000:
+            self.lastValue = 0
+
+        self.values.append(self.lastValue)
+        self.values.popleft()
 
     def readLoop(self):
         while True:
             try:
-                self.bus.write_byte_data(self.address, 0, 0x51)
-                time.sleep(0.2)
-                self.lastValue = self.bus.read_word_data(self.address, 0x02)
-
+                self.measure()
                 self.values.append(self.lastValue)
                 self.values.popleft()
             except Exception as e:
                 print(e)
 
     def read(self):
+
         mean = np.mean(self.values)
         std = np.std(self.values)
         data = [x for x in self.values if abs(x - mean) < 3 * std]
@@ -61,13 +68,28 @@ class RandomSensor(Sensor):
 
 class IO:
     def __init__(self):
-            self.sensors = [
-                SRF02(3, 0x70),
-                SRF02(3, 0x72)
-            ]
+        self.sensors = [
+            SRF02(3, 0x70),
+            SRF02(3, 0x72),
+        ]
+
+        thr = threading.Thread(target=self.pollSensors, args=(), kwargs={})
+
+        thr.start()
 
     def addSensor(self, new_sensor):
         self.sensors.append(new_sensor)
 
     def read(self):
         return [s.read() for s in self.sensors]
+
+    def pollSensors(self):
+        while True:
+            for s in self.sensors:
+                try:
+                    s.measure()
+                    print s.read(),
+                    time.sleep(0.1)
+                except Exception:
+                    print "ERR",
+            print
